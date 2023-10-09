2a5302340448a4b69e0db78838f7d824ac926357
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-10-09 16:17:59 +0200

    Add exercise 2

--- src/improvement-2.js
+++ src/improvement-2.js
@@ -5,13 +5,22 @@
  */
 
 // FUNCTION 2
-export default function processItems(items) {
-  const processedItems = [];
-  for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-    if (item.type === "Food") {
-      processedItems.push(item);
-    }
+
+// export default function processItems(items) {
+//   const processedItems = [];
+//   for (let item of items) {
+//     typeOf(item) === "Food" ? processedItems.push(item) : null;
+
+//     }
+//     return processedItems;
+//   }
+
+const processItems = (items) => {
+  let processedItems = [];
+  for (let item of items) {
+    item.type === "Food" ? processedItems.push(item) : processedItems;
   }
   return processedItems;
-}
+};
+
+export default processItems;
