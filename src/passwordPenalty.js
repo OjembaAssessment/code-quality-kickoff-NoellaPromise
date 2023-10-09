97ebee7c2dfc02bc618ea806eca7f4d6ad6ae340
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-10-09 20:00:08 +0200

    Add PasswordPenalty exercise

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -6,7 +6,6 @@
  * @returns {number}
  */
 export default function penaltyPoints(password = "") {
-  // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (password === null) return 0;
   if (typeof password !== "string") password = String(password);
 
